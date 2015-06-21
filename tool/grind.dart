35671bf311dda6cfaa33c57ad4e2a84db347e397
Kasper Peulen <kasperpeulen@gmail.com>
2015-06-21 16:16:54 +0200

    trying to get travis working

--- tool/grind.dart
+++ tool/grind.dart
@@ -5,7 +5,7 @@ const chrome = "chrome";
 const firefox = "firefox";
 const safari = "safari";
 
-const all = const ['firefox,chrome'];
+const all = const ['chrome'];
 const base = "test/base.dart";
 const polymer = "test/polymer.dart";
 const iron_collapse = "test/iron-collapse.dart";
